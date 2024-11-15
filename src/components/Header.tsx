8b10efcace45ae8f3c10ca9e5523b80df147f4c7
faisalapnshussain <faisalapnshussain@gmail.com>
2024-11-15 13:17:40 +0500

    figma assignment

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image';
+
+const Header = () => {
+  return (
+    <div className='bg-[#A29875] w-[1366px] h-[135.01px] pl-14 pt-3'>
+      <Image src="/logo.png" height={87.45} width={242.56} alt='Pic' />
+        </div>
+  )
+}
+
+export default Header;
